ca07a6b693018b8c843bcd506228b4628170e88b
joaquinperaza <joaquin@peraza.uy>
2019-08-18 23:05:03 -0300

    api jquery mobile

--- js/secundario.js
+++ js/secundario.js
@@ -1,11 +1,15 @@
-var url='http://apilayer.net/api/historical?access_key=a34e998119a429b28cc393a74d570dc1&date=2010-08-15&currencies=USD,UYU,format=1';
+var url='http://api.ipapi.com/check?access_key=0ff96fb0988bf189dbb2aeddc457ac1a&format=1';
 const api1= new XMLHttpRequest();
 $( document ).ready(function() {
 	api1.open("GET",url,true);
-	api2.send();
+	api1.send();
 	api1.onreadystatechange = function (){
 		if(this.status==200 && this.readyState==4){
 			var r=JSON.parse(this.responseText);
+			$("#ip").html("Su IP actual es: "+r.ip);
+			$("#pais").html("Pais: "+r.country_name);
+			$("#region").html("Region: "+r.region_name);
+			$("#flag").attr('src',r.location.country_flag);
 
 			
 		}
